741623edd7dd158b9cc5e2de42ab645634ca3bc9
kanxiaoxi <wzxjachinwu@gmail.com>
2021-11-22 22:37:44 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import * as qs from "qs";
+import { cleanObject } from "utils";
+import { List } from "./list"
+import { SearchPanel } from "./search-panel"
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    // ${apiUrl}/projects?name=${param.name}&personId={param.personId}
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [param]);
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    })
+  }, []);
+  return <div>
+    <SearchPanel users={users} param={param} setParam={setParam} />
+    <List users={users} list={list} />
+  </div>
+}
