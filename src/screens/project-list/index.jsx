b93e7a0d2fa1206de59471b917a7005b722d50a8
kanxiaoxi <wzxjachinwu@gmail.com>
2021-11-22 23:10:03 +0800

    feat: 添加useMount和useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import * as qs from "qs";
-import { cleanObject } from "utils";
+import { cleanObject, useDebounce, useMount } from "utils";
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 
@@ -14,22 +14,25 @@ export const ProjectListScreen = () => {
   });
 
   const [list, setList] = useState([]);
+  const debouncedParam = useDebounce(param, 2000);
 
   useEffect(() => {
     // ${apiUrl}/projects?name=${param.name}&personId={param.personId}
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if (response.ok) {
         setList(await response.json());
       }
     });
-  }, [param]);
-  useEffect(() => {
+  }, [debouncedParam]);
+
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json());
       }
     })
-  }, []);
+  });
+
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam} />
     <List users={users} list={list} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 export const isFalsy = (value) => (value === 0 ? false : !value);
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object) => {
@@ -11,3 +12,59 @@ export const cleanObject = (object) => {
   });
   return result;
 };
+
+// 第一个自定义钩子
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+/*
+React Hook useEffect has a missing dependency: 'callback'.
+Either include it or remove the dependency array.
+If 'callback' changes too often,
+find the parent component that defines it and wrap that definition in useCallback.
+ */
+
+// const debounce = (func, delay) => {
+//   let timeout;
+//   return (...param) => {
+//     if (timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function() {
+//       func(...param);
+//     }, delay);
+//   }
+// }
+// const log = debounce(() => console.log('call'), 5000)
+// log()
+// log()
+// log()
+//   ...5s
+// 执行！
+
+// debounce 原理讲解：
+// 0s ---------> 1s ---------> 2s --------> ...
+//     一定要理解：这三个函数都是同步操作，所以它们都是在 0~1s 这个时间段内瞬间完成的；
+//     log()#1 // timeout#1
+//     log()#2 // 发现 timeout#1！取消之，然后设置timeout#2
+//     log()#3 // 发现 timeout#2! 取消之，然后设置timeout#3
+//             // 所以，log()#3 结束后，就只剩timeout#3在独自等待了
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化后，设置一个定时器
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+// 不需要用到别的hook就能搞定的，写个函数就可以了，比如上面的cleanObject()。
