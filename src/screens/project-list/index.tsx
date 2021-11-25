99a27f35e943b17d5f8d66d394a80ac284185d06
kanxiaoxi <wzxjachinwu@gmail.com>
2021-11-25 21:01:12 +0800

    feat: 使用自定义hook对项目列表Loading和Error状态进行重构

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,62 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 // import * as qs from "qs";
-import { cleanObject, useDebounce, useMount } from "utils";
+import { useDebounce } from "utils";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
-import { useHttp } from "utils/http";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
-// const apiUrl = process.env.REACT_APP_API_URL;
+import { useProjects } from "utils/project";
+import { useUsers } from "utils/user";
 
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([]);
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<null | Error>(null);
-  const [list, setList] = useState([]);
+
   const debouncedParam = useDebounce(param, 200);
-  const client = useHttp();
 
-  useEffect(() => {
-    setIsLoading(true);
-    client("projects", { data: cleanObject(debouncedParam) })
-      .then(setList)
-      .catch((error) => {
-        setError(error);
-        setList([]);
-      })
-      .finally(() => setIsLoading(false));
-    // ${apiUrl}/projects?name=${param.name}&personId={param.personId}
-    // fetch(
-    //   `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    // ).then(async (response) => {
-    //   if (response.ok) {
-    //     setList(await response.json());
-    //   }
-    // });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedParam]);
-
-  useMount(() => {
-    client("users").then(setUsers);
-    // fetch(`${apiUrl}/users`).then(async (response) => {
-    //   if (response.ok) {
-    //     setUsers(await response.json());
-    //   }
-    // });
-  });
+  const { isLoading, error, data: list } = useProjects(debouncedParam);
+  const { data: users } = useUsers();
 
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel users={users} param={param} setParam={setParam} />
+      <SearchPanel users={users || []} param={param} setParam={setParam} />
       {error ? (
         <Typography.Text type={"danger"}>{error.message}</Typography.Text>
       ) : null}
-      <List users={users} dataSource={list} loading={isLoading} />
+      <List users={users || []} dataSource={list || []} loading={isLoading} />
     </Container>
   );
 };
