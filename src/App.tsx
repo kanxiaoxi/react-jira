625ae7fa814e3a154f83aba35c441d0b1b29ce35
kanxiaoxi <wzxjachinwu@gmail.com>
2021-11-25 23:24:01 +0800

    feat: 添加errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import "./App.css";
 import { useAuth } from "context/auth-context";
 import { AuthenticatedApp } from "authenticated-app";
 import { UnauthenticatedApp } from "unauthenticated-app";
+import { ErrorBoundary } from "components/error-boundary";
+import { FullPageFallback } from "components/lib";
 
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
